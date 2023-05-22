1203df00fe5ac6f0ddb84f2a215940837e16c913
Adeweh <dorcasabang@gmail.com>
2023-05-22 21:54:12 +0100

    user update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 
 
@@ -14,11 +16,32 @@ const registerUser = asyncHandler( async(req, res) =>{
         res.status(400);
         throw new Error("User already exists");
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword); 
     res.json({message: "Register the user"});
 });
 
 const loginUser = asyncHandler( async(req, res) =>{
-    res.json({message: "Login user"});
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All Fields are mandatory!");
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+           user:{
+            username: user.username,
+            email: user.email,
+            id: user.id,
+           }, 
+        }, process.env.ACCESS_TOKEN_SECRET
+        );
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401)
+        throw new Error("email or password not valid")
+    }
 });
 
 const currentUser = asyncHandler( async(req, res) =>{
